ac27d8fe9e9be2ce6e3750c13a66b463e2550bcd
Jeremy Olsen <jeremyolsen@Jeremys-Mini.hsd1.ca.comcast.net>
2023-01-25 14:32:00 -0800

    Updates to support ros2 from source

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ set -e
 
 vcs import < src/ros2.repos src
 sudo apt-get update
+sudo rosdep init
 rosdep update
-rosdep install --from-paths src --ignore-src -y
+rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
